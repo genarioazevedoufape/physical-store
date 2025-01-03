b4c43cee54cd924c8c0ea607cae92e8987cbd541
Genário Azevedo <genario.azevedo@ufape.edu.br>
2025-01-03 13:58:51 -0300

    feat: adicionando  diretório store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/physical-store'),
+    StoresModule,
   ],
   controllers: [],
   providers: [],
