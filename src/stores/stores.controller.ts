7f7ca0c53f589cfc438304c160dee5802cf6e72b
Genário Azevedo <genario.azevedo@ufape.edu.br>
2025-01-03 14:22:46 -0300

    feat: adicionando StoresController

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { StoresService } from './stores.service';
+import { Store } from './schema/store.schema';
 
 @Controller('stores')
-export class StoresController {}
+export class StoresController {
+  constructor(private readonly storesService: StoresService) {}
+
+  @Post()
+  async create(@Body() store: Partial<Store>) {
+    return this.storesService.create(store);
+  }
+
+  @Get()
+  async findAll() {
+    return this.storesService.findAll();
+  }
+
+  @Get('cep/:postalCode')
+  async findByCep(@Param('postalCode') postalCode: string) {
+    return this.storesService.findByCep(postalCode);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.storesService.findById(id);
+  }
+
+  @Get('state/:state')
+  async findByState(@Param('state') state: string) {
+    return this.storesService.findByState(state);
+  }
+}
