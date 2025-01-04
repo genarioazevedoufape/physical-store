c385d95d4dd1b91b3f7b4f93b5b0a29cef844b1d
Genário Azevedo <genario.azevedo@ufape.edu.br>
2025-01-03 21:51:25 -0300

    feat: adicionando validações no SchemaStore

--- src/stores/schema/store.schema.ts
+++ src/stores/schema/store.schema.ts
@@ -5,40 +5,49 @@ export type StoreDocument = Store & Document;
 
 @Schema()
 export class Store {
-  @Prop({ required: true })
-  storeID: string;
+  @Prop({ required: true, default: () => `STORE-${Date.now()}` })
+  storeID: string; 
 
   @Prop({ required: true })
   storeName: string;
 
-  @Prop()
-  latitude: string;
-
-  @Prop()
-  longitude: string;
-
-  @Prop()
-  state: string;
-
-  @Prop({ required: true, enum: ['PDV', 'LOJA'] })
-  type: string;
-
-  @Prop()
-  postalCode: string;
-
-  @Prop()
+  @Prop({ default: true })
+  takeOutInStore: boolean; 
+  @Prop({ required: true, min: 0 }) 
   shippingTimeInDays: number;
 
-  @Prop()
-  address1: string;
+  @Prop({ required: true })
+  latitude: string; 
 
+  @Prop({ required: true })
+  longitude: string;
+
+  @Prop({ required: true })
+  address1: string; 
   @Prop()
   address2: string;
 
   @Prop()
+  address3: string;
+
+  @Prop({ required: true })
+  city: string; 
+  @Prop({ required: true })
+  district: string; 
+
+  @Prop({ required: true })
+  state: string; 
+
+  @Prop({ required: true, enum: ['PDV', 'LOJA'] }) 
+  type: string;
+
+  @Prop({ required: true, match: /^[0-9]{5}-[0-9]{3}$/ }) 
+  postalCode: string;
+
+  @Prop({ required: true, match: /^[0-9]{10,11}$/ }) 
   telephoneNumber: string;
 
-  @Prop()
+  @Prop({ required: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }) 
   emailAddress: string;
 }
 
